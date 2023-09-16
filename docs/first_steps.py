555571225db0f3f250e2c6dcd3a7b30965fde1e8
yashgosa <yashcgosavi@gmail.com>
2023-09-16 17:12:42 +0530

    form

--- docs/first_steps.py
+++ docs/first_steps.py
@@ -499,6 +499,7 @@ app = FastAPI()
 #     return {"name": name}
 
 # =========> Form Data <=========
-
-
+@app.post('/login/')
+async def login(username: Annotated[str , Form()], password: Annotated[str, Form()]):
+    return {"username": username}
 
